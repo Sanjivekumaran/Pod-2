750d4072d6588fbc5734fcb688ce6a1ce4828e0e
kshitij5 <kshitijliveat5@gmail.com>
2022-06-29 17:06:07 +0530

    routing and service files added

--- insureity/src/app/consumer/consumer.service.ts
+++ insureity/src/app/consumer/consumer.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const CONSUMER_API_BASE_URL = 'http://localhost:8080/api/v1/employees';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConsumerService {
+  constructor(private http: HttpClient) {}
+  addConsumer(consumer: Consumer) {
+    return this.http.post<Consumer>(CONSUMER_API_BASE_URL, consumer);
+  }
 
-  constructor() { }
+  addProperty(property: Property) {
+    return this.http.post<Property>(CONSUMER_API_BASE_URL, property);
+  }
+
+  updateConsumer(consumer: Consumer) {
+    return this.http.put<Consumer>(CONSUMER_API_BASE_URL, consumer);
+  }
+
+  getConsumer(id: Number) {
+    return this.http.get<Consumer>(CONSUMER_API_BASE_URL + '/' + id);
+  }
+  getProperty(id: Number) {
+    return this.http.get<Property>(CONSUMER_API_BASE_URL + '/' + id);
+  }
 }
