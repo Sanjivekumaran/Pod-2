3113ebc0e18b38973e308358063496ad72a08e8b
kshitij5 <kshitijliveat5@gmail.com>
2022-06-29 15:18:09 +0530

    quotes module completed

--- quotes-module/src/main/java/com/cts/quotesmodule/QuotesModuleApplication.java
+++ quotes-module/src/main/java/com/cts/quotesmodule/QuotesModuleApplication.java
@@ -2,8 +2,12 @@ package com.cts.quotesmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@ComponentScan(basePackages={"com.cts"})
+@EnableJpaRepositories
 public class QuotesModuleApplication {
 
 	public static void main(String[] args) {
--- quotes-module/src/main/java/com/cts/quotesmodule/model/QuotesMaster.java
+++ quotes-module/src/main/java/com/cts/quotesmodule/model/QuotesMaster.java
@@ -1,9 +1,14 @@
 package com.cts.quotesmodule.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,14 +19,30 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "quotes")
+@Entity
+@Table(name = "quotes")
 public class QuotesMaster {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private int id;
-	private int businessValue;
-	private int PropertyValue;
-	private String PropertyType;
-	private String Quote;
+	
+	@NotNull
+	@Column(name = "Business_Value")
+	private long businessValue;
+	
+	@NotNull
+	@Column(name = "Property_Value")
+	private long propertyValue;
+	
+	@NotBlank
+	@Size(max = 50)
+	@Column(name = "Property_Type")
+	private String propertyType;
+	
+	@NotBlank
+	@Size(max = 50)
+	@Column(name = "Quotes")
+	private String quote;
 
 }
